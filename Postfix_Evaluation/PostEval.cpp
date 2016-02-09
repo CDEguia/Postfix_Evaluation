f77863b4386b620d28fbe73e0bcf8fbf97e3ba4a
Candelario Daniel Eguia <candelario.eguia@laverne.edu>
2016-02-09 13:20:32 -0800

    fixed #4 #5
    removed second Class, combined class functions, overloaded class
    functions; Added check for $

--- Postfix_Evaluation/PostEval.cpp
+++ Postfix_Evaluation/PostEval.cpp
@@ -15,89 +15,68 @@
 
 using namespace std;
 
-struct LN {
-	char a;
+struct LN{
+	char c;
 	int x;
 };
 
-class LNstack {
-private:
-	LN set[max];
-	int counter;
-public:
-	LNstack();
-	~LNstack();
-	void PushStack(char c, int x);
-	char PeekStack(int x);
-	int GetInt(int x);
-	int StackItems();
-};
-class intSTACK
+template <class  T >
+class  STACK
 {
 public:
-	intSTACK();
-	~intSTACK();
-	void PushStack(int x);
-	int PopStack();
+	STACK()
+	{
+		counter = 0;
+	}
+	~STACK()
+	{
+		counter = 0;
+	}
+	void PushStack(int n)
+	{
+		a[counter++] = n;
+	}
+	void PushStack(char z, int n) 
+	{
+		a[counter].c = z;
+		a[counter++].x = n;
+	}
+	int PopStack()
+	{
+		return a[--counter];
+	}
+	char PeekStack(int n)
+	{
+		return a[n].c;
+	}
+	int GetInt(int n)
+	{
+		return a[n].x;
+	}
+	int StackItems()
+	{
+		return counter - 1;
+	}
+private:
+	T a[max];
+	int counter;
+};
 
-private:
-	int a[max];
-	int counter;
-};
-bool isDuplicate(LNstack letter, char postfixLetter);
-bool isLetter(char a);
-void doArrithmatic(intSTACK &number, char sign);
-LNstack::LNstack()
+bool isDuplicate(STACK<LN> letter, char postfixLetter) 
 {
-	counter = 0;
-}
-void LNstack::PushStack(char c, int n)
-{
-	set[counter].a = c;
-	set[counter++].x = n;
-}
-char LNstack::PeekStack(int n)
-{
-	return set[n].a;
-}
-int LNstack::GetInt(int n)
-{
-	return set[n].x;
-}
-int LNstack::StackItems() 
-{
-	return counter - 1;
-}
-LNstack::~LNstack()
-{
-	counter = 0;
-}
-intSTACK::intSTACK()
-{
-	counter = 0;
-}
-void intSTACK::PushStack(int x)
-{
-	a[counter++] = x;
-}
-int intSTACK::PopStack()
-{
-	return a[--counter];
-}
-intSTACK::~intSTACK()
-{
-}
-bool isDuplicate(LNstack letter, char postfixLetter) {
-	for (int b = 0; b <= letter.StackItems(); ++b) {
+	for (int b = 0; b <= letter.StackItems(); ++b) 
+	{
 		if (letter.PeekStack(b) == postfixLetter) return true;
 	}
 	return false;
 }
-bool isLetter(char a) {
+bool isLetter(char a) 
+{
 	if ((a >= char('a') && a <= char('z')) || (a >= char('A') && a <= char('Z'))) return true;
 	return false;
 }
-void doArrithmatic(intSTACK &number, char sign) {
+void doArrithmatic(STACK<int> &number, char sign)
+{
 	int a;
 	switch (sign)
 	{
@@ -131,8 +110,8 @@ int main() {
 	do
 	{
 		string postfix;
-		intSTACK numbers;
-		LNstack lettersNumbers;
+		STACK<int> numbers;
+		STACK<LN> lettersNumbers;
 
 		cout << "\tEnter a postfix expression with a $ at the end: "; cin >> postfix;
 
@@ -140,19 +119,25 @@ int main() {
 
 		while (i < postfix.length())
 		{
-			if (i == postfix.length() - 1 && postfix[i] != '$') {
+			if (i == postfix.length() - 1 && postfix[i] != '$') 
+			{
 				postfix = postfix + '$';
 			}
 			int value;
 			
-			if (isLetter(postfix[i])) {
-				if (!isDuplicate(lettersNumbers, postfix[i])) {
+			if (isLetter(postfix[i])) 
+			{
+				if (!isDuplicate(lettersNumbers, postfix[i])) 
+				{
 					cout << "\t\tEnter the value of " << postfix[i] << ": "; cin >> value; numbers.PushStack(value);
 					lettersNumbers.PushStack(postfix[i], value);
 				}
-				else {
-					for (int p = 0; p <= lettersNumbers.StackItems(); p++) {
-						if (lettersNumbers.PeekStack(p) == postfix[i]) {
+				else 
+				{
+					for (int p = 0; p <= lettersNumbers.StackItems(); p++)
+					{
+						if (lettersNumbers.PeekStack(p) == postfix[i]) 
+						{
 							numbers.PushStack(lettersNumbers.GetInt(p));
 							break;
 						}
@@ -160,19 +145,19 @@ int main() {
 				}
 			}
 			else {
-				if (postfix[i] == '/' || postfix[i] == '*' || postfix[i] == '-' || postfix[i] == '+' || postfix[i] == '$') {
+				if (postfix[i] == '/' || postfix[i] == '*' || postfix[i] == '-' || postfix[i] == '+' || postfix[i] == '$') 
+				{
 					doArrithmatic(numbers, postfix[i]);
 				}
-				else {
-					cout << "Opperation not recognized!\n";
+				else 
+				{
+					cout << "Opperator not recognized!\n";
 					break;
 				}
 			}
 			i++;
 		}		
-
 		cout << "Continue (y/n)? "; cin >> cont; cont = toupper(cont);
-
 	} while (cont != 'N');
 
 	system("pause");
